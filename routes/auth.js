648ffa52a88953e93478364cdc2602519d0538ef
honghainguyen777 <n.h.hai2015@gmail.com>
2021-02-09 16:36:54 +0100

    signup

--- routes/auth.js
+++ routes/auth.js
@@ -1,26 +1,48 @@
 const router = require("express").Router();
+const User = require('../models/User.model');
+const bcrypt = require('bcrypt');
 
-// Signup
+// signup
 router.get("/signup", (req, res, next) => {
   res.render("signup");
 });
 
 // the signup form posts to this route
 router.post('/signup', (req, res) => {
+  // get username and password
   const { username, password } = req.body;
+  console.log(username, password);
   // is the password longer than 8 chars and the username not empty
-  if (password.length < 8 || password.length === 0) {
+  if (password.length < 8) {
     // if not show the signup again with a message
-    res.render("signup", {message: 'Your password must be 8 chars min'});
+    res.render('signup', { message: 'Your password must be 8 chars min' });
   }
-
-  // check if the username already exists
   if (username === '') {
-    res.render("signup", {message: 'Your username can not be empty'});
+    res.render('signup', { message: 'Your username cannot be empty' });
   }
-  // if yes show the signup again with a message
-  // all validation passed - > we create a new user in the database with a hashed password
-  // then redirect to login
+  // check if the username already exists
+  User.findOne({ username: username })
+    .then(userFromDB => {
+      if (userFromDB !== null) {
+        // if yes show the signup again with a message
+        res.render('signup', { message: 'Username is already taken' });
+      } else {
+        // all validation passed - > we can create a new user in the database with a hashed password
+        // create salt and hash
+        const salt = bcrypt.genSaltSync();
+        const hash = bcrypt.hashSync(password, salt)
+        // create the user in the db
+        User.create({ username: username, password: hash })
+          .then(userFromDB => {
+            console.log(userFromDB);
+            // then redirect to login
+            res.redirect('/');
+          });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
